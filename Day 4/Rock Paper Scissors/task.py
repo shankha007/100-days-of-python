3051773222c739a9dfc374c5d07b8aebb1d58ee2
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-05-28 21:57:49 +0530

    Day 4 done

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,23 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+choices = [rock, paper, scissors]
+
+human_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
+
+if human_choice < 0 or human_choice > 2:
+    print("Wrong choice! Try again.")
+else:
+    comp_choice = random.randint(0, 2)
+    print(choices[human_choice])
+
+    print("Computer chose:")
+    print(choices[comp_choice])
+
+    if human_choice == comp_choice:
+        print("Draw")
+    elif (human_choice == 0 and comp_choice == 2) or (human_choice == 1 and comp_choice == 0) or (human_choice == 2 and comp_choice == 1):
+        print("You won.")
+    else:
+        print("You lose.")
