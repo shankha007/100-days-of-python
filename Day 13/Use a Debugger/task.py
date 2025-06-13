d1807ab2e351aecae935708ef9cd8605134b23bf
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-06-13 20:35:54 +0530

    Day 13 done

--- Day 13/Use a Debugger/task.py
+++ Day 13/Use a Debugger/task.py
@@ -1,7 +1,6 @@
 import random
 import maths
 
-
 def mutate(a_list):
     b_list = []
     new_item = 0
@@ -9,7 +8,7 @@ def mutate(a_list):
         new_item = item * 2
         new_item += random.randint(1, 3)
         new_item = maths.add(new_item, item)
-    b_list.append(new_item)
+        b_list.append(new_item)
     print(b_list)
 
 
