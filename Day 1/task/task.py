e4d9b01b90e6d2a85e5e8c4a3fccc44ebe5c5ce2
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-06-14 20:33:13 +0530

    Refactoring

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,4 +1,4 @@
-print("Welcome to the Band Name Generator.")
+eprint("Welcome to the Band Name Generator.")
 city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
 
