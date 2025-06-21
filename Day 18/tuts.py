2bbe056e1539c3cb8b6271010777f85702300b8d
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-06-21 13:21:54 +0530

    Day 18 refactor

--- Day 18/tuts.py
+++ Day 18/tuts.py
@@ -20,7 +20,7 @@ def draw_spirograph(size_of_gap):
         tim.circle(100)
         tim.setheading(tim.heading() + size_of_gap)
 
-draw_spirograph(5)
+draw_spirograph(6)
 
 screen = t.Screen()
 screen.exitonclick()
