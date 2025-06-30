364a198006b650c25458fb0dc669f044dcfbb326
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-07-01 00:57:50 +0530

    Day 29 refactoring

--- Day 29/main.py
+++ Day 29/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
@@ -28,25 +29,60 @@ def password_generator():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    website_value = website_input.get()
+    website_value = website_input.get().lower()
     email_value = email_input.get()
     password_value = password_input.get()
+
+    new_data = {
+        website_value: {
+            "email": email_value,
+            "password": password_value
+        }
+    }
 
     if len(website_value) == 0 or len(email_value) == 0 or len(password_value) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        combined_string = f"{website_value} | {email_value} | {password_value}"
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
 
-        is_okay = messagebox.askokcancel(title=website_value,
-                                         message=f"These are the details entered: \nEmail: {email_value} \nPassword: {password_value} \nIs it okay to save?")
-
-        if is_okay:
-            with open("data.txt", "a") as file:
-                file.write(combined_string + "\n")
-
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_input.delete(0, END)
             password_input.delete(0, END)
             website_input.focus()
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def search_password():
+    website_value = website_input.get().lower()
+
+    if len(website_value) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave Website field empty!")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Oops", message="Start with storing some values.")
+        else:
+            if website_value in data:
+                email_value = data[website_value]["email"]
+                password_value = data[website_value]["password"]
+                messagebox.showinfo(title=website_value.title(),
+                                    message=f"Email: {email_value} \nPassword: {password_value}")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for {website_value.title()} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,9 +99,12 @@ website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
 website_input = Entry()
-website_input.config(width=56)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.config(width=37)
+website_input.grid(row=1, column=1)
 website_input.focus()
+
+search_button = Button(text="Search", command=search_password)
+search_button.grid(row=1, column=2)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
