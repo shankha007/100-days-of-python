05df666a0f2141f1848b951b295d93035bdc6083
Shankha Shubhra Das <ribhumaster@gmail.com>
2025-07-01 00:16:27 +0530

    Day 26 refactoring

--- Day 26/main.py
+++ Day 26/main.py
@@ -3,7 +3,14 @@ import pandas
 df = pandas.DataFrame(pandas.read_csv("./nato_phonetic_alphabet.csv"))
 phonetic_dictionary = {row.letter: row.code for (index, row) in df.iterrows()}
 
-user_name = input("Enter your name? ").upper()
+def get_phonetic_codes():
+    user_name = input("Enter your name? ").upper()
 
-phonetic_alphabet_code = [phonetic_dictionary[letter] for letter in user_name]
-print(phonetic_alphabet_code)
+    try:
+        phonetic_alphabet_code = [phonetic_dictionary[letter] for letter in user_name]
+        print(phonetic_alphabet_code)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        get_phonetic_codes()
+
+get_phonetic_codes()
